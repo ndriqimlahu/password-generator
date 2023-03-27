b3ea2860edb3370c4a3eba82bebe917ac5bd3d73
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2023-03-27 18:35:40 +0200

    Integrated the Navigation component into Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,11 @@
 import classes from "./Header.module.css";
+import Navigation from "./Navigation";
 
 const Header = (props) => {
   return (
     <header className={classes["main-header"]}>
       <a href="/">{props.applicationName}</a>
+      <Navigation />
     </header>
   );
 };

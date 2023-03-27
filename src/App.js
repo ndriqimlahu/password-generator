f335f42acf2a0f5699ad0fe340be0b98070bb9eb
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2023-03-27 18:18:27 +0200

    Using the props and useState Hook for components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./App.css";
 import Card from "./components/UI/Card";
 import PasswordResult from "./components/Tool/PasswordResult";
@@ -6,12 +8,25 @@ import PasswordSettings from "./components/Tool/PasswordSettings";
 import Button from "./components/UI/Button";
 
 function App() {
+  const [enterResult, setResult] = useState("Click on the Generate button");
+  const [changeLength, setLength] = useState(16);
+  const [useLowercase, setLowercase] = useState(false);
+  const [useUppercase, setUppercase] = useState(false);
+  const [useNumber, setNumber] = useState(false);
+  const [useSymbol, setSymbol] = useState(false);
+  const [checkboxesChecked, setCheckboxesChecked] = useState(false);
+
   return (
     <Card>
-      <PasswordResult />
-      <PasswordLength />
-      <PasswordSettings />
-      <Button />
+      <PasswordResult result={enterResult} />
+      <PasswordLength length={changeLength} />
+      <PasswordSettings
+        lowercase={useLowercase}
+        uppercase={useUppercase}
+        number={useNumber}
+        symbol={useSymbol}
+      />
+      <Button handleCheckboxes={checkboxesChecked} />
     </Card>
   );
 }

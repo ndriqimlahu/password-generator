115aac4595b1a85a47754c7a9f652ce254001b81
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2023-03-27 18:14:53 +0200

    Integrated the all components into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
 import Card from "./components/UI/Card";
+import PasswordResult from "./components/Tool/PasswordResult";
+import PasswordLength from "./components/Tool/PasswordLength";
+import PasswordSettings from "./components/Tool/PasswordSettings";
+import Button from "./components/UI/Button";
 
 function App() {
   return (
-    <Card></Card>
+    <Card>
+      <PasswordResult />
+      <PasswordLength />
+      <PasswordSettings />
+      <Button />
+    </Card>
   );
 }
 
